174c6442c72a96c96456f67dffbda8b3c7fc7bfd
gqqnbig <gqqnb2005@gmail.com>
2019-04-08 14:44:33 -0700

    Allow single parameter and reduce trace files size
    Usage:
    profile.py ./
    
    The script will read corresponding files in the specified folder, and figure out pid. Thus the original trace file can be deleted, in order to save disk space.

--- profiler.py
+++ profiler.py
@@ -10,33 +10,58 @@ import dash_core_components as dcc
 import dash_html_components as dhc
 import pandas as pd
 
+import os
+import re
 
 if __name__ == '__main__':
     # log_path = './logs/mar19-1/trace-chrome-1'
     # pid = xxxx
 
-    log_path = sys.argv[1]
-    pid = int(sys.argv[2])
+    pid = 0
+    heapFileName = ''
+    gcFileName = ''
+    memFileName = ''
 
-    lg.clean(log_path, log_path+'-clean')
-    lg.heap_filter(log_path+'-clean', log_path+'-heap')
-    lg.gc_filter(log_path+'-clean', log_path+'-gc')
-    lg.sys_gc_filter(log_path+'-clean', log_path+'-sysgc')
-    lg.mem_filter(log_path+'-clean', log_path+'-mem')
+    if len(sys.argv) == 2:
+        for file in os.listdir(sys.argv[1]):
+            if re.search("-\\d+-heap.csv$", file):
+                heapFileName = os.path.join(sys.argv[1], file)
+            elif re.search("-\\d+-gc.csv$", file):
+                gcFileName = os.path.join(sys.argv[1], file)
+            elif re.search("-\\d+-mem.csv$", file):
+                memFileName = os.path.join(sys.argv[1], file)
 
-    lg.heap_csv(log_path + '-heap', log_path + f'-{pid}-heap.csv', pid)
-    lg.gc_csv(log_path + '-gc', log_path + f'-{pid}-gc.csv', pid)
-    lg.mem_csv(log_path + '-mem', log_path + f'-{pid}-mem.csv', pid)
+        pid = re.search("-(\\d)+-heap.csv$", heapFileName).group(1)
+    elif len(sys.argv) == 3:
+        log_path = sys.argv[1]
+        pid = int(sys.argv[2])
 
-    heap_csv = pd.read_csv(log_path + f'-{pid}-heap.csv')
+        heapFileName = log_path + f'-{pid}-heap.csv'
+        gcFileName = log_path + f'-{pid}-gc.csv'
+        memFileName = log_path + f'-{pid}-mem.csv'
+
+        lg.clean(log_path, log_path + '-clean')
+        lg.heap_filter(log_path + '-clean', log_path + '-heap')
+        lg.gc_filter(log_path + '-clean', log_path + '-gc')
+        lg.sys_gc_filter(log_path + '-clean', log_path + '-sysgc')
+        lg.mem_filter(log_path + '-clean', log_path + '-mem')
+
+        lg.heap_csv(log_path + '-heap', heapFileName, pid)
+        lg.gc_csv(log_path + '-gc', gcFileName, pid)
+        lg.mem_csv(log_path + '-mem', memFileName, pid)
+    else:
+        print("Incorrect number of parameters.")
+        sys.exit(0)
+
+    heap_csv = pd.read_csv(heapFileName)
     heap_tshres = heap_csv.tshres
     heap_heap_num_bytes_allocated = heap_csv.heap_num_bytes_allocated
 
-    gc_csv = pd.read_csv(log_path + f'-{pid}-gc.csv')
+    gc_csv = pd.read_csv(gcFileName)
     gc_tshres = gc_csv.tshres
     gc_gc_bytes_freed = gc_csv.gc_bytes_freed
 
-    mem_csv = pd.read_csv(log_path + f'-{pid}-mem.csv')
+    mem_csv = pd.read_csv(memFileName)
     mem_tshres = mem_csv.tshres
     mem_iget = mem_csv.iget
     mem_iput = mem_csv.iput
